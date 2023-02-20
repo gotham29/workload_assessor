25b70812d7ee62c23d86122980e8fed651dde967
Sam Heiserman <sheiser1@binghamton.edu>
2023-02-19 20:26:10 -0800

    debug - heatmap

--- source/pipeline/run_pipeline.py
+++ source/pipeline/run_pipeline.py
@@ -245,11 +245,9 @@ def make_save_plots(dir_out,
     # df_ttypesdiffs
     df_ttypesdiffs = pd.concat(dfs_ttypesdiffs, axis=0)
     df_ttypesdiffs.to_csv(os.path.join(dir_out, f"WL_Diffs.csv"))
-    print("df_ttypesdiffs COLS")
-    print(f"  --> {df_ttypesdiffs.columns}")
-    img = sns.heatmap(df_ttypesdiffs, annot=True).get_figure()
+    cols_plot = [c for c in df_ttypesdiffs if c != 'subject']
+    img = sns.heatmap(df_ttypesdiffs[cols_plot], annot=True).get_figure()
     img.savefig(os.path.join(dir_out, f"WL_Diffs-heatmap.png"))
-
     # subjects WLdiffs
     plot_bars(mydict=subjects_wldiffs,
               title=f'WL Change from WL Levels 0 to 1-3\n  Total % Change={round(diff_from_WL0, 3)}',
